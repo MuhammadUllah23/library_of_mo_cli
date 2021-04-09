175abbbbfec20122fe2e745430a8346ecc9d1807
Muhammad <muhammadhullah23@gmail.com>
2021-04-09 11:17:12 -0400

    greetings method

--- lib/library_of_mo_cli/cli.rb
+++ lib/library_of_mo_cli/cli.rb
@@ -1,9 +1,16 @@
 class CLI
     def start
-        puts "Welcome to Mo's Library."
+        puts "Please enter your name:"
+        name = user_input
+        greeting(name)
     end
 
     def user_input
         gets.strip
+    end
+
+    def greeting(name)
+        puts "Welcome to Mo's Library #{name}! You'll find books of all categories ranging from fiction to non-fiction, from business books and how-to's to magazines and manga."
+        puts "Would you like to see the list of categories?"
     end
 end
